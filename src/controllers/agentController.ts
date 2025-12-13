319aa9deaea6f96eedcf190a8ac91a4da3db5d3d
0 <leoferreira1712@gmail.com>
2025-12-13 14:47:09 +0000

    Add project settings example JSON

--- src/controllers/agentController.ts
+++ src/controllers/agentController.ts
@@ -1,6 +1,12 @@
 import { type Request, type Response } from 'express'
 
+export const getModels = async (req: Request, res: Response) => {
+  // TODO: call the getModels from all services and return it
+  res.json({ success: true, message: `List models here` })
+}
+
 export const ask = async (req: Request, res: Response) => {
-  const { name } = req.params
-  res.json({ success: true, message: `hello you called agent ${name}` })
+  const { model, prompt } = req.body
+
+  res.json({ success: true, message: `POST request received, ${model}` })
 }
--- src/routes/agentRoute.ts
+++ src/routes/agentRoute.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import { ask } from '../controllers/agentController'
+import { getModels, ask } from '../controllers/agentController'
 
 export const agentRouter = express.Router()
 
-agentRouter.get('/', ask)
+agentRouter.get('/', getModels)
+agentRouter.post('/ask', ask)
